10b4014e8dbe825159e8ead4f1e1a7dec8a4fd39
Viktor Rudenko <v.rudenko@triacom.ua>
2023-05-29 23:01:02 +0300

    before express

---
+++ 23-NodeJs/index.js
@@ -0,0 +1,12 @@
+// const superheroes = require("superheroes");
+import superheroes from "superheroes";
+import supervillians from "supervillains"
+
+const mySuperVillainName = console.log(superheroes.random());
+
+// console.log(mySuperHeroName);
+console.log(mySuperVillainName);
+
+// const mySuperHeroName = console.log(superheroes.random());
+
+// console.log(mySuperHeroName);
